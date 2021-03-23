70c8c70f49e2f0b51cd13f0ce6676b5e38198a3c
zimice <simon.kochanek@me.com>
2021-03-23 17:58:57 +0100

    First sketch

--- run.sh
+++ run.sh
@@ -1,2 +1,7 @@
 #!/bin/sh
-sudo apt-get install -y nginx libnginx-mod-rtmp > /dev/null
+sudo apt-get install -y nginx libnginx-mod-rtmp  /dev/null
+sudo rm /etc/nginx/nginx.conf
+sudo mv nginx.conf etc/nginx/
+sudo nginx -t
+sudo systemctl start nginx
+sudo systemct enable nginx
